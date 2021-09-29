84bb60bd969333afcdf821267534bbfd1d2c93cf
雷承憲 <leichengxian@tpea90114642.whq.wistron>
2021-09-29 13:06:24 +0800

    Hello final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
+import { ReactComponent as ErrorIcon } from './images/icon-error.svg'
 
 function clsx(...str) {
-  console.log(str);
+  // console.log(str);
   return str.filter(Boolean).join(" ");
 };
 
@@ -13,28 +14,44 @@ function Card({children, className}) {
   );
 }
 
-function TextField({ id, label }) {
+function TextField({ id, label, error }) {
   const [value, setValue] = useState('');
-
+  console.log(error);
   // htmlFor will indicate to input's id and name indicate to form submit 
   return (
-    <div className="relative flex items-center">
-      <label htmlFor={id} className="absolute px-3 text-sm font-medium">
-        {value ? '' : label}
-      </label> 
-      <input
-        type="text"
-        className={clsx(
-          "outline-none",
-          "border border-opacity-10 w-full p-3 rounded",
-          "border-blue-dark focus:border-blue"
-          )}
-        id={id}
-        name={id}
-        onChange={(event) => setValue(event.target.value)}
-        value={value}
-      />
+    <div>
+      <div className={clsx("relative flex items-center", error && "text-red")}>
+        <label
+          htmlFor={id}
+          className={clsx(
+            "absolute px-3",
+            "text-sm font-medium"
+          )}>
+          {value ? '' : label}
+        </label> 
+        <input
+          type="text"
+          className={clsx(
+            "outline-none",
+            "border w-full p-3 rounded",
+            error ? "border-red" :  "border-blue-dark focus:border-blue border-opacity-10",
+            )}
+          id={id}
+          name={id}
+          onChange={(event) => setValue(event.target.value)}
+          value={value}
+        />
+        {error && (
+           <ErrorIcon className="absolute right-0 w-6 mr-3"/>
+        )}
+      </div>
+      {error && (
+        <div className="flex justify-end text-sm text-red">
+          <span>{error}</span>
+        </div>
+      )}
     </div>
+
   );
 }
 
@@ -49,10 +66,37 @@ function Button({children, className}) {
 
 function App() {
 
+  const [formState, setFormState] = useState([
+    { id: "first-name", error: false, errorMsg: "First Name cannot be empty", label: "First Name" },
+    { id: "last-name", error: false, errorMsg: "Last Name cannot be empty", label: "Last Name" },
+    { id: "email", error: false, errorMsg: "Looks like this is not an email", label: "Email" },
+    { id: "password", error: false, errorMsg: "Password cannot be empty", label: "Password" },
+  ]);
+
+  /**
+   * 
+   * @param {Event} event 
+   */
+  
+  function onSubmit(event) {
+
+    event.preventDefault();
+
+    const form = new FormData(event.target);
+    // destruct data by input's name
+    const data = Object.fromEntries(form.entries());
+    // console.log(data);
+    setFormState((formstate) => (
+      formstate.map((state) => ({
+        ...state,
+        error: !Boolean(data[state.id])
+      }))
+    ));
+  }
+
   return (
     // 這裡可以用container 也可以用max-w-7xl來代替
     <div className="h-full text-white px-4 gap-16 flex flex-col md:flex-row md:items-center container mx-auto">
-      {/* articel */}
       <article className="pt-24 md:pt-0 text-center md:text-left space-y-6 md:flex-1">
         <h1 className="font-bold text-3xl">Learn to code by watching others</h1>
         <p>
@@ -61,26 +105,22 @@ function App() {
       </article>
 
       <section className="grid gap-6 md:flex-1">
-        {/* form title */}
         <Card className="bg-blue">
           <p className="px-8">Try it free 7 days then $20/mo. thereafter</p>
         </Card>
-        {/* form */}
         <Card className="bg-white text-blue-dark mb-32 md:mb-0">
-          <form action="" className="space-y-4">
-            {/* First Name */}
-            <TextField id="first-name" label="First Name"/>
-            {/* Last Name */}
-            <TextField id="last-name" label="Last Name"/>
-            {/* Email Address */}
-            <TextField id="email" label="Email"/>
-            {/* Password */}
-            <TextField id="password" label="Password"/>
-            {/* submit */}
+          <form className="space-y-4" onSubmit={onSubmit}>
+            {formState.map(({id, label, errorMsg, error}) => (
+              <TextField
+                key={id}
+                id={id}
+                label={label}
+                error={error && errorMsg}
+              />
+            ))}
             <Button className="text-white">
               CLAIM YOUR FREE TRIAL
             </Button>
-            {/* terms */}
             <div>
               <p className="text-gray text-xs px-4">
                 By clicking the button, you are agreeing to our
